48eeff3d7281bdefdb0ffb92afea2f880a2c979c
Ashutosh <ashutosh.shukla@innovect.com>
2021-01-02 15:59:59 +0530

    1.SASS support done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+import ComB from "./ComB";
+import ComC from "./ComC";
+import { createContext } from "react";
+
+const FirstName = createContext("");
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="testSass">Test sass dependencies.</div>
+      <FirstName.Provider value={"Ashutosh"}>
+        <ComC />
+      </FirstName.Provider>
+    </>
   );
 };
 
 export default App;
+export { FirstName };
---
+++ src/ComC.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ComC = () => {
+  return <div>Hell my name is Ashutosh Shukla</div>;
+};
+
+export default ComC;
