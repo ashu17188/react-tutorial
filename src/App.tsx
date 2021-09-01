a8ee4acfe5e1a3d2b7496e8f784b50ce951ba8a9
Ashutosh <ashutosh.shukla@innovect.com>
2021-09-01 21:45:13 +0530

    Add external api call.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Signup from "./Components/Signup";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { AuthProvider } from "./contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
+import Dashboard from "./Components/Dashboard/Dashboard";
 import Login from "./Components/Login";
 import PrivateRoute from "./Components/PrivateRoute";
 import ForgotPassword from "./Components/ForgotPassword";
@@ -11,23 +11,18 @@ import UpdateProfile from "./Components/UpdateProfile";
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-            </Switch>
-          </AuthProvider>
-        </Router>
-      </div>
+    <Container fluid="md">
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+          </Switch>
+        </AuthProvider>
+      </Router>
     </Container>
   );
 }
---
+++ src/Components/Dashboard/Dashboard.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  Button,
+  Alert,
+  ListGroup,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
+import { useAuth } from "../../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
+import { UserService } from "./UserService";
+import { User } from "./Model/User";
+
+export default function Dashboard() {
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
+
+  async function handleLogout() {
+    setError("");
+
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => res.json())
+      .then((data: User) => setUser({ name: data.name, email: data.email }));
+  }, []);
+
+  return (
+    <>
+      <Row>
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Profile</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <strong>Email:</strong> {currentUser.email}
+              <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+                Update Profile
+              </Link>
+              <div className="w-100 text-center mt-2">
+                <Button variant="link" onClick={handleLogout}>
+                  Log Out
+                </Button>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <Alert variant="primary">
+                {user.name},{user.email}
+              </Alert>
+              <h2>User's List</h2>
+              <ListGroup>
+                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+}
